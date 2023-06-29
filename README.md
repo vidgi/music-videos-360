993dfe7fc19f77b3994a6c433b6f160ed70ec9b7
vidya <vzg805@gmail.com>
2023-06-29 02:49:51 -0500

    making mobile view

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 experimenting with displaying 60 music videos in a 3d space
 
+<img width="1000" alt="music-videos-360" src="https://vidyagiri.com/images/mv-360.gif">
+
 - to run local: `yarn start`
 - to build: `yarn build`
 - to deploy: `yarn run deploy`
 
 packages:
+
 - [three.js](https://threejs.org/)
 - [react-three-fiber](https://github.com/pmndrs/react-three-fiber)
 - [drei](https://github.com/pmndrs/drei)
--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import "./App.css";
 
-import { Suspense, useRef } from 'react'
+import { Suspense, useRef } from "react";
 
 import { Canvas, useFrame } from "react-three-fiber";
-import { Image, Environment, Loader,FirstPersonControls,PointerLockControls, Html, OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import {
+  Image,
+  Environment,
+  Loader,
+  FirstPersonControls,
+  PointerLockControls,
+  Html,
+  OrbitControls,
+  PerspectiveCamera,
+} from "@react-three/drei";
 
 function App() {
   const videos = [
@@ -67,90 +76,87 @@ function App() {
     "uIjU0do0PqU",
     "2KkMyDSrBVI",
     "pIz2K3ArrWk",
-  ]
+  ];
   return (
     <div className="App">
-       <Canvas style={{ height: "100vh", width: "100vw" }}>
-       <Suspense fallback={null}>
-         
+      <Canvas style={{ height: "100vh", width: "100vw" }}>
+        <Suspense fallback={null}>
+          <group transform position={[20, 0, -20]}>
+            {/* <YoutubeThumbnail link = {videos[0]}/> */}
 
-        <group transform position={[20, 0, -20]}>
-         {/* <YoutubeThumbnail link = {videos[0]}/> */}
+            <YouTubeEmbed position={[-5, -20, 51]} link={videos[0]} rotation={[0 * (Math.PI / 180), -150 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-5, -10, 51]} link={videos[1]} rotation={[0 * (Math.PI / 180), -150 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-5, 0, 51]} link={videos[2]} rotation={[0 * (Math.PI / 180), -150 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-5, 10, 51]} link={videos[3]} rotation={[0 * (Math.PI / 180), -150 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-5, 20, 51]} link={videos[4]} rotation={[0 * (Math.PI / 180), -150 * (Math.PI / 180), 0]} />
 
-          <YouTubeEmbed position={[-5, -20, 51]} link = {videos[0]} rotation={[0 * (Math.PI / 180), -150 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-5, -10, 51]} link = {videos[1]} rotation={[0 * (Math.PI / 180), -150 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-5, 0, 51]} link = {videos[2]} rotation={[0 * (Math.PI / 180), -150 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-5, 10, 51]} link = {videos[3]} rotation={[0 * (Math.PI / 180), -150 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-5, 20, 51]} link = {videos[4]} rotation={[0 * (Math.PI / 180), -150 * (Math.PI / 180), 0]}/>
+            <YouTubeEmbed position={[6, -20, 40]} link={videos[5]} rotation={[0 * (Math.PI / 180), -120 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[6, -10, 40]} link={videos[6]} rotation={[0 * (Math.PI / 180), -120 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[6, 0, 40]} link={videos[7]} rotation={[0 * (Math.PI / 180), -120 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[6, 10, 40]} link={videos[8]} rotation={[0 * (Math.PI / 180), -120 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[6, 20, 40]} link={videos[9]} rotation={[0 * (Math.PI / 180), -120 * (Math.PI / 180), 0]} />
 
-          <YouTubeEmbed position={[6, -20, 40]} link = {videos[5]} rotation={[0 * (Math.PI / 180), -120 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[6, -10, 40]} link = {videos[6]} rotation={[0 * (Math.PI / 180), -120 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[6, 0, 40]} link = {videos[7]} rotation={[0 * (Math.PI / 180), -120 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[6, 10, 40]} link = {videos[8]} rotation={[0 * (Math.PI / 180), -120 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[6, 20, 40]} link = {videos[9]} rotation={[0 * (Math.PI / 180), -120 * (Math.PI / 180), 0]}/>
+            <YouTubeEmbed position={[10, -20, 25]} link={videos[10]} rotation={[0 * (Math.PI / 180), -90 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[10, -10, 25]} link={videos[11]} rotation={[0 * (Math.PI / 180), -90 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[10, 0, 25]} link={videos[12]} rotation={[0 * (Math.PI / 180), -90 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[10, 10, 25]} link={videos[13]} rotation={[0 * (Math.PI / 180), -90 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[10, 20, 25]} link={videos[14]} rotation={[0 * (Math.PI / 180), -90 * (Math.PI / 180), 0]} />
 
-          <YouTubeEmbed position={[10, -20, 25]} link = {videos[10]} rotation={[0 * (Math.PI / 180), -90 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[10, -10, 25]} link = {videos[11]} rotation={[0 * (Math.PI / 180), -90 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[10, 0, 25]} link = {videos[12]} rotation={[0 * (Math.PI / 180), -90 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[10, 10, 25]} link = {videos[13]} rotation={[0 * (Math.PI / 180), -90 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[10, 20, 25]} link = {videos[14]} rotation={[0 * (Math.PI / 180), -90 * (Math.PI / 180), 0]}/>
+            <YouTubeEmbed position={[6, -20, 10]} link={videos[15]} rotation={[0 * (Math.PI / 180), -60 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[6, -10, 10]} link={videos[16]} rotation={[0 * (Math.PI / 180), -60 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[6, 0, 10]} link={videos[17]} rotation={[0 * (Math.PI / 180), -60 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[6, 10, 10]} link={videos[18]} rotation={[0 * (Math.PI / 180), -60 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[6, 20, 10]} link={videos[19]} rotation={[0 * (Math.PI / 180), -60 * (Math.PI / 180), 0]} />
 
-          <YouTubeEmbed position={[6, -20, 10]} link = {videos[15]} rotation={[0 * (Math.PI / 180), -60 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[6, -10, 10]} link = {videos[16]} rotation={[0 * (Math.PI / 180), -60 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[6, 0, 10]} link = {videos[17]} rotation={[0 * (Math.PI / 180), -60 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[6, 10, 10]} link = {videos[18]} rotation={[0 * (Math.PI / 180), -60 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[6, 20, 10]} link = {videos[19]} rotation={[0 * (Math.PI / 180), -60 * (Math.PI / 180), 0]}/>
+            <YouTubeEmbed position={[-5, -20, -1]} link={videos[20]} rotation={[0 * (Math.PI / 180), -30 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-5, -10, -1]} link={videos[21]} rotation={[0 * (Math.PI / 180), -30 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-5, 0, -1]} link={videos[22]} rotation={[0 * (Math.PI / 180), -30 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-5, 10, -1]} link={videos[23]} rotation={[0 * (Math.PI / 180), -30 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-5, 20, -1]} link={videos[24]} rotation={[0 * (Math.PI / 180), -30 * (Math.PI / 180), 0]} />
 
-          <YouTubeEmbed position={[-5, -20, -1]} link={videos[20]} rotation={[0 * (Math.PI / 180), - 30 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-5, -10, -1]} link={videos[21]} rotation={[0 * (Math.PI / 180), - 30 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-5, 0, -1]} link={videos[22]} rotation={[0 * (Math.PI / 180), - 30 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-5, 10, -1]} link={videos[23]} rotation={[0 * (Math.PI / 180), - 30 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-5, 20, -1]} link={videos[24]} rotation={[0 * (Math.PI / 180), - 30 * (Math.PI / 180), 0]}/>
+            <YouTubeEmbed position={[-20, -20, -5]} link={videos[25]} rotation={[0 * (Math.PI / 180), 0 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-20, -10, -5]} link={videos[26]} rotation={[0 * (Math.PI / 180), 0 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-20, 0, -5]} link={videos[27]} rotation={[0 * (Math.PI / 180), 0 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-20, 10, -5]} link={videos[28]} rotation={[0 * (Math.PI / 180), 0 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-20, 20, -5]} link={videos[29]} rotation={[0 * (Math.PI / 180), 0 * (Math.PI / 180), 0]} />
 
-          <YouTubeEmbed position={[-20, -20, -5]} link = {videos[25]} rotation={[0 * (Math.PI / 180), 0 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-20, -10, -5]} link = {videos[26]} rotation={[0 * (Math.PI / 180), 0 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-20, 0, -5]} link = {videos[27]} rotation={[0 * (Math.PI / 180), 0 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-20, 10, -5]} link = {videos[28]} rotation={[0 * (Math.PI / 180), 0 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-20, 20, -5]} link = {videos[29]} rotation={[0 * (Math.PI / 180), 0 * (Math.PI / 180), 0]}/>
+            <YouTubeEmbed position={[-35, -20, -1]} link={videos[30]} rotation={[0 * (Math.PI / 180), 30 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-35, -10, -1]} link={videos[31]} rotation={[0 * (Math.PI / 180), 30 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-35, 0, -1]} link={videos[32]} rotation={[0 * (Math.PI / 180), 30 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-35, 10, -1]} link={videos[33]} rotation={[0 * (Math.PI / 180), 30 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-35, 20, -1]} link={videos[34]} rotation={[0 * (Math.PI / 180), 30 * (Math.PI / 180), 0]} />
 
-          <YouTubeEmbed position={[-35, -20, -1]} link = {videos[30]} rotation={[0 * (Math.PI / 180), 30 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-35, -10, -1]} link = {videos[31]} rotation={[0 * (Math.PI / 180), 30 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-35, 0, -1]} link = {videos[32]} rotation={[0 * (Math.PI / 180), 30 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-35, 10, -1]} link = {videos[33]} rotation={[0 * (Math.PI / 180), 30 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-35, 20, -1]} link = {videos[34]} rotation={[0 * (Math.PI / 180), 30 * (Math.PI / 180), 0]}/>
+            <YouTubeEmbed position={[-46, -20, 10]} link={videos[35]} rotation={[0 * (Math.PI / 180), 60 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-46, -10, 10]} link={videos[36]} rotation={[0 * (Math.PI / 180), 60 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-46, 0, 10]} link={videos[37]} rotation={[0 * (Math.PI / 180), 60 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-46, 10, 10]} link={videos[38]} rotation={[0 * (Math.PI / 180), 60 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-46, 20, 10]} link={videos[39]} rotation={[0 * (Math.PI / 180), 60 * (Math.PI / 180), 0]} />
 
-          <YouTubeEmbed position={[-46, -20, 10]} link = {videos[35]} rotation={[0 * (Math.PI / 180), 60 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-46, -10, 10]} link = {videos[36]} rotation={[0 * (Math.PI / 180), 60 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-46, 0, 10]} link = {videos[37]} rotation={[0 * (Math.PI / 180), 60 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-46, 10, 10]} link = {videos[38]} rotation={[0 * (Math.PI / 180), 60 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-46, 20, 10]} link = {videos[39]} rotation={[0 * (Math.PI / 180), 60 * (Math.PI / 180), 0]}/>
+            <YouTubeEmbed position={[-50, -20, 25]} link={videos[40]} rotation={[0 * (Math.PI / 180), 90 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-50, -10, 25]} link={videos[41]} rotation={[0 * (Math.PI / 180), 90 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-50, 0, 25]} link={videos[42]} rotation={[0 * (Math.PI / 180), 90 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-50, 10, 25]} link={videos[43]} rotation={[0 * (Math.PI / 180), 90 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-50, 20, 25]} link={videos[44]} rotation={[0 * (Math.PI / 180), 90 * (Math.PI / 180), 0]} />
 
-          <YouTubeEmbed position={[-50, -20, 25]} link = {videos[40]} rotation={[0 * (Math.PI / 180), 90 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-50, -10, 25]} link = {videos[41]} rotation={[0 * (Math.PI / 180), 90 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-50, 0, 25]} link = {videos[42]} rotation={[0 * (Math.PI / 180), 90 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-50, 10, 25]} link = {videos[43]} rotation={[0 * (Math.PI / 180), 90 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-50, 20, 25]} link = {videos[44]} rotation={[0 * (Math.PI / 180), 90 * (Math.PI / 180), 0]}/>
+            <YouTubeEmbed position={[-46, -20, 40]} link={videos[45]} rotation={[0 * (Math.PI / 180), 120 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-46, -10, 40]} link={videos[46]} rotation={[0 * (Math.PI / 180), 120 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-46, 0, 40]} link={videos[47]} rotation={[0 * (Math.PI / 180), 120 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-46, 10, 40]} link={videos[48]} rotation={[0 * (Math.PI / 180), 120 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-46, 20, 40]} link={videos[49]} rotation={[0 * (Math.PI / 180), 120 * (Math.PI / 180), 0]} />
 
-          <YouTubeEmbed position={[-46, -20, 40]} link = {videos[45]} rotation={[0 * (Math.PI / 180), 120 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-46, -10, 40]} link = {videos[46]} rotation={[0 * (Math.PI / 180), 120 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-46, 0, 40]} link = {videos[47]} rotation={[0 * (Math.PI / 180), 120 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-46, 10, 40]} link = {videos[48]} rotation={[0 * (Math.PI / 180), 120 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-46, 20, 40]} link = {videos[49]} rotation={[0 * (Math.PI / 180), 120 * (Math.PI / 180), 0]}/>
+            <YouTubeEmbed position={[-35, -20, 51]} link={videos[50]} rotation={[0 * (Math.PI / 180), 150 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-35, -10, 51]} link={videos[51]} rotation={[0 * (Math.PI / 180), 150 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-35, 0, 51]} link={videos[52]} rotation={[0 * (Math.PI / 180), 150 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-35, 10, 51]} link={videos[53]} rotation={[0 * (Math.PI / 180), 150 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-35, 20, 51]} link={videos[54]} rotation={[0 * (Math.PI / 180), 150 * (Math.PI / 180), 0]} />
 
-          <YouTubeEmbed position={[-35, -20, 51]} link = {videos[50]} rotation={[0 * (Math.PI / 180), 150 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-35, -10, 51]} link = {videos[51]} rotation={[0 * (Math.PI / 180), 150 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-35, 0, 51]} link = {videos[52]} rotation={[0 * (Math.PI / 180), 150 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-35, 10, 51]} link = {videos[53]} rotation={[0 * (Math.PI / 180), 150 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-35, 20, 51]} link = {videos[54]} rotation={[0 * (Math.PI / 180), 150 * (Math.PI / 180), 0]}/>
-
-          <YouTubeEmbed position={[-20, -20, 55]} link = {videos[55]} rotation={[0 * (Math.PI / 180), 180 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-20, -10, 55]} link = {videos[56]} rotation={[0 * (Math.PI / 180), 180 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-20, 0, 55]} link = {videos[57]} rotation={[0 * (Math.PI / 180), 180 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-20, 10, 55]} link = {videos[58]} rotation={[0 * (Math.PI / 180), 180 * (Math.PI / 180), 0]}/>
-          <YouTubeEmbed position={[-20, 20, 55]} link = {videos[59]} rotation={[0 * (Math.PI / 180), 180 * (Math.PI / 180), 0]}/>
-
-        </group>
-        {/* <Environment 
+            <YouTubeEmbed position={[-20, -20, 55]} link={videos[55]} rotation={[0 * (Math.PI / 180), 180 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-20, -10, 55]} link={videos[56]} rotation={[0 * (Math.PI / 180), 180 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-20, 0, 55]} link={videos[57]} rotation={[0 * (Math.PI / 180), 180 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-20, 10, 55]} link={videos[58]} rotation={[0 * (Math.PI / 180), 180 * (Math.PI / 180), 0]} />
+            <YouTubeEmbed position={[-20, 20, 55]} link={videos[59]} rotation={[0 * (Math.PI / 180), 180 * (Math.PI / 180), 0]} />
+          </group>
+          {/* <Environment 
         background={true}
         files={[
           `https://cdn.jsdelivr.net/gh/mrdoob/three.js@r123/examples/textures/cube/angus/cube_m00_c00.jpg`,
@@ -161,14 +167,13 @@ function App() {
           `https://cdn.jsdelivr.net/gh/mrdoob/three.js@r123/examples/textures/cube/angus/cube_m00_c05.jpg`,
         ]}
         /> */}
-        
         </Suspense>
         <ambientLight />
         {/* <FirstPersonControls
           movementSpeed={0}
           lookSpeed= {0.02}
           /> */}
-          <PointerLockControls/>
+        <PointerLockControls />
         {/* <OrbitControls 
          minDistance={20}
           maxDistance={20}
@@ -176,10 +181,8 @@ function App() {
         // autoRotateSpeed={0.4}
           />
          <PerspectiveCamera position={[-20, 0, -20]} /> */}
-
       </Canvas>
       <Loader />
-
     </div>
   );
 }
@@ -190,11 +193,17 @@ function YouTubeEmbed(props) {
   return (
     <Html transform position={props.position} rotation={props.rotation}>
       {/* <p>{props.link}</p> */}
-      <img src={"https://img.youtube.com/vi/"+props.link+"/0.jpg"} alt="thumbnail" width="560" height="315"/>
-      {/* <iframe width="560" height="315" src={"https://www.youtube.com/embed/" + props.link} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
+      {/* <img src={"https://img.youtube.com/vi/" + props.link + "/0.jpg"} alt="thumbnail" width="560" height="315" /> */}
+      <iframe
+        width="560"
+        height="315"
+        src={"https://www.youtube.com/embed/" + props.link}
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
     </Html>
-
-    
   );
 }
 
